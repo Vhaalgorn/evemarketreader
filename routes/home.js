6f15423cfc7ab41e21e9d9c857d8a48c2ac9bdcf
Vhaalgorn <rafaelvianafranco@hotmail.com>
2023-02-07 02:20:13 +0000

    Data pulling from mysql complete
    Database query works, this produces a complex nested object that shows
    the complete production chain for any blueprint. Further testing is
    require to verify that all blueprints work. It is now possible to engage
    in displaying the data to the user.

--- routes/home.js
+++ routes/home.js
@@ -8,56 +8,72 @@ const connection = mysql.createConnection({
     password:'password',
     database:'evemarketreader'
 })
+var searchString = [{blueprint_typeID:11394}] //at this point the typeID will always be a blueprint_typeID
+var searchString2 = 11394
+connection.connect((err)=>{if (err) throw err; console.log('MySQL server connected.')})
 
-var searchString = '11394' //at this point the typeID will always be a blueprint_typeID
-
-connection.connect((err)=>
-{
-    if (err) throw err;
-    console.log('MySQL server connected.');
-})
-async function get_blueprintTypeID(blueprint_typeID)
-{
-    let result = await new Promise((resolve, reject)=>
-    {
-        
-        connection.query(`SELECT typeID as blueprint_typeID FROM industryActivityProducts WHERE productTypeID=${blueprint_typeID}`, (err,result)=>
-        {
+async function get_blueprintTypeID(item_typeID){
+    let result = await new Promise((resolve, reject)=>{
+        connection.query(`SELECT typeID as blueprint_typeID FROM industryActivityProducts WHERE productTypeID=${item_typeID}`, (err,result)=>{
             if (err) reject(err)
-            else
-            {
-                if(result[0] === undefined){
-                    resolve('material') 
-                }
+            else{
+                if(result[0] === undefined){resolve('material')}
                 else resolve(result[0].blueprint_typeID); 
             }
         })
-        
     })
     return result;
 }
 
-async function get_material_TypeID(blueprint_typeID)
-{
-    let result = await new Promise((resolve,reject)=>
-    {
-        connection.query(`SELECT * FROM industryActivityMaterials WHERE typeID=${blueprint_typeID} and activityID=1`, (err,result)=>
-        {
+async function get_material_TypeID(blueprint_typeID){
+    //returns a object containing all item_typeID for needed for blueprint_typeID
+    let result = await new Promise((resolve,reject)=>{
+        connection.query(`
+
+        SELECT invTypes.typeID, invTypes.groupID, invTypes.typeName, invGroups.groupName, industryActivityMaterials.materialTypeID, industryActivityMaterials.quantity, industryActivityProducts.productTypeID
+        FROM invTypes
+        inner join industryActivityMaterials on invTypes.typeID=industryActivityMaterials.typeID
+        inner join industryActivityProducts on invTypes.typeID=industryActivityProducts.typeID
+        inner join invGroups on invTypes.groupID = invGroups.groupID
+        where invTypes.published = 1 and industryActivityMaterials.activityID =1 and invTypes.typeID = ${blueprint_typeID} or invTypes.published = 1 and industryActivityMaterials.activityID =11 and invTypes.typeID = ${blueprint_typeID};`, (err,result)=>{
             if (err) reject(err)
             else{resolve(result)}
         })
     })
-    for (material in result)
-    {
-        result[material].blueprint = await get_blueprintTypeID(Number(result[material].materialTypeID))
+    
+    //converts item_typeID in the object and returns a (number) blueprint_typeID of that item. 
+    for (let material in result){
+        result[material].blueprint_typeID = await get_blueprintTypeID(Number(result[material].materialTypeID))
     }
     return result;
 }
 
+async function iterateObj(obj)
+{
+    let result = await get_material_TypeID(obj)
+    
+    for (let item in result)
+    {
+        if(result[item].blueprint_typeID=='material'){}
+        else
+        {
+            result[item].blueprint_typeID = await iterateObj(result[item].blueprint_typeID)
+        }
+        
+    }
+
+    return result
+}
+
+
 router.get('/',async (req, res)=>
 {
-    let result = await get_material_TypeID(searchString)
-    //let result = await get_blueprintTypeID(11399)
-    res.send(result)
+    //let result = await iterateBranch(searchString)
+    //let result = await get_blueprintTypeID(11393)
+    let result = await iterateObj(searchString2)
+    //let result = await get_material_TypeID(11394)
+    //let result = await get_data(11394)
+    //console.log(result)
+    res.end(JSON.stringify(result, null, 3));
 })
 module.exports = router
