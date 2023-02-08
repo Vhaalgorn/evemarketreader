d4390dbeddd004116cd5c2eaebb77fd193da9b55
Vhaalgorn <rafaelvianafranco@hotmail.com>
2023-02-08 13:15:35 +0000

    mysql query iterator complete
    The iterator is complete, which returns a nested object that contains
    all the materials and resources needed for a blueprint.
    Seems to work with any t1 and t2 blueprint except
    for t3 blueprints (for obviours reasons). The next step is to implement
    a way to fetch all the resources in the object into a handy list.
    
    Now we can work on a

--- routes/home.js
+++ routes/home.js
@@ -8,13 +8,13 @@ const connection = mysql.createConnection({
     password:'password',
     database:'evemarketreader'
 })
-var searchString = [{blueprint_typeID:11394}] //at this point the typeID will always be a blueprint_typeID
-var searchString2 = 11394
+var searchString = [{blueprint_typeID:12024}] //at this point the typeID will always be a blueprint_typeID
+var searchString2 = 28711
 connection.connect((err)=>{if (err) throw err; console.log('MySQL server connected.')})
 
 async function get_blueprintTypeID(item_typeID){
     let result = await new Promise((resolve, reject)=>{
-        connection.query(`SELECT typeID as blueprint_typeID FROM industryActivityProducts WHERE productTypeID=${item_typeID}`, (err,result)=>{
+        connection.query(`SELECT industryActivityProducts.typeID as blueprint_typeID FROM industryActivityProducts inner join invTypes on industryActivityProducts.typeID=invTypes.typeID WHERE productTypeID=${item_typeID} and invTypes.published = 1`, (err,result)=>{
             if (err) reject(err)
             else{
                 if(result[0] === undefined){resolve('material')}
@@ -35,7 +35,7 @@ async function get_material_TypeID(blueprint_typeID){
         inner join industryActivityMaterials on invTypes.typeID=industryActivityMaterials.typeID
         inner join industryActivityProducts on invTypes.typeID=industryActivityProducts.typeID
         inner join invGroups on invTypes.groupID = invGroups.groupID
-        where invTypes.published = 1 and industryActivityMaterials.activityID =1 and invTypes.typeID = ${blueprint_typeID} or invTypes.published = 1 and industryActivityMaterials.activityID =11 and invTypes.typeID = ${blueprint_typeID};`, (err,result)=>{
+        where invTypes.published = 1 and industryActivityMaterials.activityID = 1 and invTypes.typeID = ${blueprint_typeID} or invTypes.published = 1 and industryActivityMaterials.activityID = 11 and invTypes.typeID = ${blueprint_typeID};`, (err,result)=>{
             if (err) reject(err)
             else{resolve(result)}
         })
@@ -52,12 +52,15 @@ async function iterateObj(obj)
 {
     let result = await get_material_TypeID(obj)
     
+    
     for (let item in result)
     {
         if(result[item].blueprint_typeID=='material'){}
         else
         {
+            
             result[item].blueprint_typeID = await iterateObj(result[item].blueprint_typeID)
+            //console.log(result[item].blueprint_typeID)
         }
         
     }
